7b5c85b521244df59f5b4faab2aa6196fc0e3c89
stablelane <aaanshad09@gmail.com>
2024-10-24 19:52:50 +0530

    refactor code and add event delegation

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const taskSubmitBtn = document.getElementById('task-submit-btn')
 const taskText = document.getElementById('task-text')
 const taskList = document.getElementById('task-list')
 
+document.addEventListener('click', (e) => {
+    //listening for taskSumbitBtn
+    if (e.target.id === 'task-submit-btn') {
+        makeTask(e)
+    } 
+    //listening for click on Checkbox and call strikeTask function
+    else if (e.target.classList.value === 'checkbox') {
+        strikeTask(e.target.id)
+    }
+    //listening for click on DeleteBtn and call deleteTask function
+    else if (e.target.classList.value === 'delete-btn') {
+        deleteTask(e.target.id)
+    }
+})
 
-//task creation
-taskSubmitBtn.addEventListener('click',makeTask)
+
 let taskID = 1111
 
 function makeTask(e) {
-        e.preventDefault()
-        createTask(taskText.value)
-        clearInput()
+    e.preventDefault()
+    createTask(taskText.value)
+    clearInput()
 }
-
 function createTask(text) {
-    taskList.innerHTML +=  `<div id='${text}-task' class="task">
-                                <input id='${text}-checkbox' class='checkbox' type="checkbox">
-                                <p id='${text}-text'>${text}</p>
-                                <img id="${text}-delete" class='delete-btn' src="icon/bin.png" alt="">
+    taskList.innerHTML += `<div id='${taskID}-task' class="task">
+                                <input id='${taskID}' class='checkbox' type="checkbox">
+                                <p id='${taskID}-taskID'>${text}</p>
+                                <img id="${taskID}" class='delete-btn' src="icon/bin.png" alt="">
                             </div>`
-    addTaskBtnFunc(text)
+    taskID++
 }
 
 function clearInput() {
     taskText.value = ''
 }
 
-function addTaskBtnFunc(id) {
-    const checkbox = document.querySelectorAll('.checkbox')
-    checkbox.forEach(item => item.addEventListener('click', () => {
-        document.getElementById(`${id}-text`).classList.add('strike')
-    }))
-    const deleteBtn = document.querySelectorAll('.delete-btn')
-    deleteBtn.forEach(item => item.addEventListener('click', () => {
-        document.getElementById(`${id}-task`).outerHTML = ''
-    }))
- 
+function deleteTask(id) {
+    document.getElementById(`${id}-task`).outerHTML = ''
 }
+
+function strikeTask(id) {
+    document.getElementById(`${id}-task`).classList.toggle('strike')
+}
+
 
 
